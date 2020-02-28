1a5524bff2679f175d2d9d2bbd71bd132236680e
Doensel <dan.weissen@gmail.com>
2020-02-28 11:22:44 +0100

    readded AndroidManifest.xml

---
+++ lib/ad_related/ad_id.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_admob/firebase_admob.dart';
+
+String getAdAppId() => BannerAd.testAdUnitId;
+String getBannerAdUnitId() => BannerAd.testAdUnitId;
+String getInterstitialAdUnitId() => BannerAd.testAdUnitId;
+String getRewardAdUnitId() => BannerAd.testAdUnitId;
