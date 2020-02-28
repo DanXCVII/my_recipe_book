607b954b48b5afa7271bf18ac0685660860eb1a5
Doensel <dan.weissen@gmail.com>
2020-02-28 11:36:04 +0100

    deleted ad related stuff

--- lib/ad_related/ad_id.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_admob/firebase_admob.dart';
-
-String getAdAppId() => BannerAd.testAdUnitId;
-String getBannerAdUnitId() => BannerAd.testAdUnitId;
-String getInterstitialAdUnitId() => BannerAd.testAdUnitId;
-String getRewardAdUnitId() => BannerAd.testAdUnitId;
