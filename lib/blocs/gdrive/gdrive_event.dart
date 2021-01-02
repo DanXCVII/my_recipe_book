333ffa0e0fc6d64837497098be38f61a852ac365
Daniel Weißen <dan.weissen@gmail.com>
2021-01-02 12:58:29 +0100

    added basic gdrive infrastructure

---
+++ lib/blocs/gdrive/gdrive_event.dart
@@ -0,0 +1,8 @@
+part of 'gdrive_bloc.dart';
+
+abstract class GdriveEvent extends Equatable {
+  const GdriveEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/blocs/gdrive/gdrive_state.dart
@@ -0,0 +1,10 @@
+part of 'gdrive_bloc.dart';
+
+abstract class GdriveState extends Equatable {
+  const GdriveState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class GdriveInitial extends GdriveState {}
